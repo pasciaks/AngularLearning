98829d83e0cca0ada060a724fefecfc8959cc3d7
Sheldon Pasciak <sheldon@pasciak.com>
2024-07-19 13:19:32 -0600

    Debugging exercise complete

--- DEBUGGING_section_five_debugging-starting-project/src/app/app.component.ts
+++ DEBUGGING_section_five_debugging-starting-project/src/app/app.component.ts
@@ -3,17 +3,17 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from "./tasks/tasks.component";
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
-  users = DUMMY_USERS;
+  users: { id: string; name: string; avatar: string }[] = DUMMY_USERS;
   selectedUserId?: string;
 
   get selectedUser() {
