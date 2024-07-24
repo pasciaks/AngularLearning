2e134bdfb841a69665a56fb2afe30236ff2b739e
Sheldon Pasciak <sheldon@pasciak.com>
2024-07-24 15:05:39 -0600

    Interceptors complete

--- 12-http-starting-project/src/main.ts
+++ 12-http-starting-project/src/main.ts
@@ -1,8 +1,36 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpEventType,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { tap } from 'rxjs';
+
+function loggingInterceptor(
+  request: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) {
+  const req = request.clone({
+    // headers: request.headers.set('X-DEBUG', 'interceptor-added'),
+  });
+  console.log('LOGGING INTERCEPTOR\n\nRequest:', req);
+  return next(req).pipe(
+    tap({
+      next: (event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('LOGGING INTERCEPTOR\nRESPONSE INTERCEPTION:\n', req);
+          console.log('Response:', event.status);
+          console.log('Response body:', event.body);
+        }
+      },
+    })
+  );
+}
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(withInterceptors([loggingInterceptor]))],
 }).catch((err) => console.error(err));
