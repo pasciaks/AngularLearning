b29d6b51277d13ed40751efafbe99d5160edbf01
Sheldon Pasciak <sheldon@pasciak.com>
2024-07-24 08:27:34 -0600

    Misc

---
+++ 10c-removing-use-of-zone-js/src/app/messages/messages-list/messages-list.component.ts
@@ -0,0 +1,65 @@
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  DestroyRef,
+  inject,
+  input,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-messages-list',
+  standalone: true,
+  templateUrl: './messages-list.component.html',
+  styleUrl: './messages-list.component.css',
+  imports: [AsyncPipe],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+// TRIGERRING CHANGE DETECTION MANUALLY
+export class MessagesListComponent /* implements OnInit, OnDestroy */ {
+  ////private destroyRef = inject(DestroyRef);
+
+  // messages = input.required<string[]>();
+  ////private cdRef = inject(ChangeDetectorRef);
+
+  private messagesService = inject(MessagesService);
+
+  messages = this.messagesService.allMessages;
+
+  // Alternate version, no need for destroyRef, etc
+  // Asynchronous pipe in the template will handle the subscription
+  // altVersionMessages$ = this.messagesService.messages$;
+
+  // ngOnDestroy(): void {
+  //   console.log('MessagesListComponent in ngOnDestroy destroyed.');
+  // }
+
+  // ngOnInit(): void {
+  //   // TRIGERRING CHANGE DETECTION MANUALLY
+  //   const subscription = this.messagesService.messages$.subscribe(
+  //     (messages: string[]) => {
+  //       this.messages = messages;
+  //       this.cdRef.markForCheck();
+  //     }
+  //   );
+  //   // CLEAN UP SUSCRIPTION ON DESTROY
+  //   this.destroyRef.onDestroy(() => {
+  //     console.log('MessagesListComponentin destroy ref destroyed.');
+  //     subscription.unsubscribe();
+  //   });
+  // }
+
+  // get messages() {
+  //   return this.messagesService.allMessages;
+  // }
+
+  get debugOutput() {
+    console.log('[MessagesList] "debugOutput" binding re-evaluated.');
+    return 'MessagesList Component Debug Output';
+  }
+}
